dc5e1b36a2cc688946131bc4d247310e0321ad02
Gia Minh <Minh@DESKTOP-26O1A6C>
2023-10-06 22:51:13 +0700

    Fix link to logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -31,7 +31,7 @@ const Header = () => {
           <div className="row align-items-center">
             <div className="col-2">
               <h2>
-                <Link className='text-white'>HMart</Link>
+                <Link to='/' className='text-white'>HMart</Link>
               </h2>
             </div>
             <div className="col-5">
