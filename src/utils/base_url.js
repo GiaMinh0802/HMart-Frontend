d77b6f5ea30d35d3565bb09772d4eb537d88e687
GiaMinh <vogiaminh0802@gmail.com>
2023-11-08 01:00:04 +0700

    Change URL

--- src/utils/base_url.js
+++ src/utils/base_url.js
@@ -1,2 +1,2 @@
-export const base_url = 'http://localhost:8080/api/'
-// export const base_url = 'https://hmart-backend.onrender.com/api/'
+// export const base_url = 'http://localhost:8080/api/'
+export const base_url = 'https://hmart-backend.onrender.com/api/'
