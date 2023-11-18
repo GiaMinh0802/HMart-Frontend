1bdf231af20ef15e94f88ed7ced6c002280c4e73
GiaMinh0802 <93763817+GiaMinh0802@users.noreply.github.com>
2023-11-18 22:07:23 +0700

    Change url

--- src/utils/base_url.js
+++ src/utils/base_url.js
@@ -1,2 +1,2 @@
-export const base_url = 'http://localhost:8080/api/'
+export const base_url = 'http://207.148.120.240:8080/api/'
 // export const base_url = 'https://hmart-backend.onrender.com/api/'
